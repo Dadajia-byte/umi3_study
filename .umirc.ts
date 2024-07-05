0ef9127f09549c00f89bd36820c89abad1a4160c
unknown <as.chen@shunwang.com>
2024-07-05 17:57:42 +0800

    config

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
-  nodeModulesTransform: {
-    type: 'none',
+  nodeModulesTransform: {// node_modules 目录下依赖文件
+    type: 'none', // all 慢 兼容性好，none 快 兼容性一般
   },
   routes: [
     { path: '/', component: '@/pages/index' },
   ],
-  fastRefresh: {},
+  // fastRefresh: {}, // 快速刷新，以保持组件状态，同时编辑提供即时反馈
 });
